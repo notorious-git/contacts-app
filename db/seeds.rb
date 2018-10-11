be356fa298f2c7441615c84d6480b610c2150ff2
Peter Jang <peter.x.jang@gmail.com>
2018-10-10 21:31:33 -0500

    Add a seeds file to setup the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Contact.create(first_name: "Peter", last_name: "Jang", email: "peter@email.com", phone_number: "3432432432")
+Contact.create(first_name: "Jay", last_name: "Wengrow", email: "jay@email.com", phone_number: "3432432433")
+Contact.create(first_name: "Russell", last_name: "Mstowski", email: "russell@email.com", phone_number: "9432432433")
+Contact.create(first_name: "Adam", last_name: "Goldwater", email: "adam@email.com", phone_number: "9032432433")
