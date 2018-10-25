52d5cd9f4cfc8ba792ae71ade737c619d82b34e8
Peter Jang <peter.x.jang@gmail.com>
2018-10-24 19:17:30 -0500

    Add user/contact associations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,6 +3,8 @@ class Contact < ApplicationRecord
   validates :last_name, presence: true
   validates :email, uniqueness: true
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
+  belongs_to :user
   
   def friendly_updated_at
     updated_at.strftime("%B %e, %Y")
